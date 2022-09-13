c04709ae2f5f5e9052ac5bcfb11df382c92ae1fa
RobinSrk <robinsrk3@gmail.com>
2022-09-13 23:16:55 +0600

    updated design and file names

--- src/main/java/com/example/quizmanagement/MainApplication.java
+++ src/main/java/com/example/quizmanagement/MainApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class MainApplication extends Application {
 	@Override
 	public void start(Stage stage) throws IOException {
-		FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("hello-view.fxml"));
+		FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
 		Scene scene = new Scene(fxmlLoader.load(), 820, 640);
 		stage.setTitle("Online java quiz management system");
 		stage.setScene(scene);
