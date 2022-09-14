fcf78bb1815fd597eeba8bf1e9f4694d76711fe5
RobinSrk <robinsrk3@gmail.com>
2022-09-14 12:49:38 +0600

    added back button

--- src/main/java/com/example/quizmanagement/AddQuestionController.java
+++ src/main/java/com/example/quizmanagement/AddQuestionController.java
@@ -1,4 +1,24 @@
 package com.example.quizmanagement;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class AddQuestionController {
+	@FXML
+	private Button goBack;
+	
+	@FXML
+	private void onClickGoBack() throws Exception{
+		Stage stage = (Stage) goBack.getScene().getWindow();
+		stage.close();
+		Stage primaryStage = new Stage();
+		Parent root = FXMLLoader.load(getClass().getResource("main-view.fxml"));
+		primaryStage.setTitle("Add question");
+		primaryStage.setScene(new Scene(root, 800, 500));
+		primaryStage.show();
+	}
 }
