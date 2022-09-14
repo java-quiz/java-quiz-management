2263ba63fa6ff69dcdd875ccaebd0482ac2fbb28
RobinSrk <robinsrk3@gmail.com>
2022-09-14 12:41:05 +0600

    added images and functionalities

---
+++ src/main/java/com/example/quizmanagement/AddQuestionController.java
@@ -0,0 +1,4 @@
+package com.example.quizmanagement;
+
+public class AddQuestionController {
+}
--- src/main/java/com/example/quizmanagement/MainApplication.java
+++ src/main/java/com/example/quizmanagement/MainApplication.java
@@ -11,7 +11,7 @@ public class MainApplication extends Application {
 	@Override
 	public void start(Stage stage) throws IOException {
 		FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
-		Scene scene = new Scene(fxmlLoader.load(), 520, 340);
+		Scene scene = new Scene(fxmlLoader.load(), 800, 500);
 		stage.setTitle("Online java quiz management system");
 		stage.setScene(scene);
 		stage.show();
