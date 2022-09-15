f7fb25eaccc09f2e97ef5e7cafaac156e5080b9b
RobinSrk <robinsrk3@gmail.com>
2022-09-15 17:06:04 +0600

    fetch options and code update

--- src/main/java/com/example/quizmanagement/QuizController.java
+++ src/main/java/com/example/quizmanagement/QuizController.java
@@ -6,6 +6,9 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -13,8 +16,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
 
 public class QuizController {
 	
@@ -38,15 +39,29 @@ public class QuizController {
 	
 	@FXML
 	private void onClickStart() throws Exception {
-		List<Label> questionList = new ArrayList<>();
 		Class.forName("com.mysql.jdbc.Driver");
 		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/quiz", "root", "");
 		Statement st = con.createStatement();
 		ResultSet rs = st.executeQuery("Select * from quiz_list");
 		for (int i = 0; rs.next(); i++) {
-			questionList.add(new Label(i + 1 + ". " + rs.getString("Question")));
+			quizBox.getChildren().add(new Label(i + 1 + ". " + rs.getString("Question")));
+			HBox opBox = new HBox();
+			RadioButton o1 = new RadioButton(rs.getString("option1"));
+			RadioButton o2 = new RadioButton(rs.getString("option2"));
+			RadioButton o3 = new RadioButton(rs.getString("option3"));
+			RadioButton o4 = new RadioButton(rs.getString("option4"));
+			ToggleGroup optionGroup = new ToggleGroup();
+			o1.setToggleGroup(optionGroup);
+			o2.setToggleGroup(optionGroup);
+			o3.setToggleGroup(optionGroup);
+			o4.setToggleGroup(optionGroup);
+			opBox.getChildren().add(o1);
+			opBox.getChildren().add(o2);
+			opBox.getChildren().add(o3);
+			opBox.getChildren().add(o4);
+			quizBox.getChildren().add(opBox);
+			
 		}
-		quizBox.getChildren().addAll(questionList);
 		con.close();
 		st.close();
 		rs.close();
