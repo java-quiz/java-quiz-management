800f39313b2f87c0429176626582b27e99aed306
RobinSrk <robinsrk3@gmail.com>
2022-09-17 19:54:05 +0600

    added functionality for adding questions

--- src/main/java/com/example/quizmanagement/AddQuestionController.java
+++ src/main/java/com/example/quizmanagement/AddQuestionController.java
@@ -6,18 +6,38 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 
 public class AddQuestionController {
 	@FXML
 	private Button goBack;
 	
 	@FXML
+	private TextArea question;
+	
+	@FXML
+	private TextField op1;
+	@FXML
+	private TextField op2;
+	@FXML
+	private TextField op3;
+	@FXML
+	private TextField op4;
+	@FXML
+	private TextField cro;
+	@FXML
 	private Label clickText;
 	@FXML
 	private Button submit;
+	
 	@FXML
-	private void onClickGoBack() throws Exception{
+	private void onClickGoBack() throws Exception {
 		Stage stage = (Stage) goBack.getScene().getWindow();
 		stage.close();
 		Stage primaryStage = new Stage();
@@ -26,8 +46,19 @@ public class AddQuestionController {
 		primaryStage.setScene(new Scene(root, 400, 600));
 		primaryStage.show();
 	}
+	
 	@FXML
-	private void onClickSubmit() {
-		clickText.setText("Button clicked");
+	private void onClickSubmit() throws Exception {
+		if (op1.getText().equals("") || op2.getText().equals("") || op3.getText().equals("") || op4.getText().equals("") || cro.getText().equals("") || question.getText().equals("")) {
+			System.out.println("Null value");
+		} else if (cro.getText().equals(op1.getText()) || cro.getText().equals(op2.getText()) || cro.getText().equals(op3.getText()) || cro.getText().equals(op4.getText())) {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/quiz", "root", "");
+			Statement st = con.createStatement();
+			st.executeUpdate("INSERT INTO `quiz_list` (`Question`, `option1`, `option2`, `option3`, `option4`, `correct`) VALUES ('" + question.getText() + "', '" + op1.getText() + "', '" + op2.getText() + "', '" + op3.getText() + "', '" + op4.getText() + "', '" + cro.getText() + "');");
+		} else {
+			System.out.println("Error");
+		}
+		
 	}
 }
