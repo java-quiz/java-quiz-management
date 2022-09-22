d802b76f0686a6b0d708bedc358e79b283af1384
RobinSrk <robinsrk3@gmail.com>
2022-09-22 13:26:34 +0600

    reformat code

--- src/main/java/com/example/quizmanagement/AddQuestionController.java
+++ src/main/java/com/example/quizmanagement/AddQuestionController.java
@@ -17,60 +17,61 @@ import java.sql.DriverManager;
 import java.sql.Statement;
 
 public class AddQuestionController {
-    @FXML
-    private Button goBack;
+	@FXML
+	private Button goBack;
 
 
-    @FXML
-    private Label error;
-    @FXML
-    private TextArea question;
+	@FXML
+	private Label error;
+	@FXML
+	private TextArea question;
 
-    @FXML
-    private VBox container;
-    @FXML
-    private TextField op1;
-    @FXML
-    private TextField op2;
-    @FXML
-    private TextField op3;
-    @FXML
-    private TextField op4;
-    @FXML
-    private TextField cro;
-    @FXML
-    private Label clickText;
-    @FXML
-    private Button submit;
+	@FXML
+	private VBox container;
+	@FXML
+	private TextField op1;
+	@FXML
+	private TextField op2;
+	@FXML
+	private TextField op3;
+	@FXML
+	private TextField op4;
+	@FXML
+	private TextField cro;
+	@FXML
+	private Label clickText;
+	@FXML
+	private Button submit;
 
-    @FXML
-    private void onClickGoBack() throws Exception {
-        Stage stage = (Stage) goBack.getScene().getWindow();
-        stage.close();
-        Stage primaryStage = new Stage();
-        Parent root = FXMLLoader.load(getClass().getResource("main-view.fxml"));
-        primaryStage.setTitle("Online Java Quiz Management System");
-        primaryStage.setScene(new Scene(root, 400, 600));
-        primaryStage.show();
-    }
+	@FXML
+	private void onClickGoBack() throws Exception {
+		Stage stage = (Stage) goBack.getScene().getWindow();
+		stage.close();
+		Stage primaryStage = new Stage();
+		Parent root = FXMLLoader.load(getClass().getResource("main-view.fxml"));
+		root.getStylesheets().add(getClass().getResource("/com/example/quizmanagement/styles.css").toExternalForm());
+		primaryStage.setTitle("Online Java Quiz Management System");
+		primaryStage.setScene(new Scene(root, 400, 600));
+		primaryStage.show();
+	}
 
-    @FXML
-    private void onClickSubmit() throws Exception {
-        if (op1.getText().equals("") || op2.getText().equals("") || op3.getText().equals("") || op4.getText().equals("") || cro.getText().equals("") || question.getText().equals("")) {
-            error.setText("Please fill all the fields");
-        } else if (!cro.getText().equals(op1.getText()) || !cro.getText().equals(op2.getText()) || !cro.getText().equals(op3.getText()) || !cro.getText().equals(op4.getText())) {
-            error.setText("Correct ans doesn't match any of the above options");
-        } else {
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/quiz", "root", "");
-            Statement st = con.createStatement();
-            if (st.executeUpdate("INSERT INTO `quiz_list` (`Question`, `option1`, `option2`, `option3`, `option4`, `correct`) VALUES ('" + question.getText() + "', '" + op1.getText() + "', '" + op2.getText() + "', '" + op3.getText() + "', '" + op4.getText() + "', '" + cro.getText() + "');") == 1) {
-                container.getChildren().clear();
-                container.getChildren().add(new Label("Question added successfully"));
-                container.getChildren().add(goBack);
-                container.setAlignment(Pos.CENTER);
-            }
-        }
+	@FXML
+	private void onClickSubmit() throws Exception {
+		if (op1.getText().equals("") || op2.getText().equals("") || op3.getText().equals("") || op4.getText().equals("") || cro.getText().equals("") || question.getText().equals("")) {
+			error.setText("Please fill all the fields");
+		} else if (!cro.getText().equals(op1.getText()) || !cro.getText().equals(op2.getText()) || !cro.getText().equals(op3.getText()) || !cro.getText().equals(op4.getText())) {
+			error.setText("Correct ans doesn't match any of the above options");
+		} else {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/quiz", "root", "");
+			Statement st = con.createStatement();
+			if (st.executeUpdate("INSERT INTO `quiz_list` (`Question`, `option1`, `option2`, `option3`, `option4`, `correct`) VALUES ('" + question.getText() + "', '" + op1.getText() + "', '" + op2.getText() + "', '" + op3.getText() + "', '" + op4.getText() + "', '" + cro.getText() + "');") == 1) {
+				container.getChildren().clear();
+				container.getChildren().add(new Label("Question added successfully"));
+				container.getChildren().add(goBack);
+				container.setAlignment(Pos.CENTER);
+			}
+		}
 
-    }
+	}
 }
--- src/main/java/com/example/quizmanagement/MainApplication.java
+++ src/main/java/com/example/quizmanagement/MainApplication.java
@@ -11,12 +11,12 @@ public class MainApplication extends Application {
 	public static void main(String[] args) {
 		launch();
 	}
-	
+
 	@Override
 	public void start(Stage stage) throws IOException {
 		FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
 		Scene scene = new Scene(fxmlLoader.load(), 400, 600);
-		scene.getStylesheets().add(getClass().getResource("/com/example/quizmanagement/main.css").toExternalForm());
+		scene.getStylesheets().add(getClass().getResource("/com/example/quizmanagement/styles.css").toExternalForm());
 		stage.setTitle("Online Java Quiz Management System");
 		stage.setScene(scene);
 		stage.show();
