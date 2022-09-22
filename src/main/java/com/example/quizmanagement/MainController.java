41c9171385e4dff86bad094bb6ddf4669072a8b3
RobinSrk <robinsrk3@gmail.com>
2022-09-22 13:25:34 +0600

    updated login conditions

--- src/main/java/com/example/quizmanagement/MainController.java
+++ src/main/java/com/example/quizmanagement/MainController.java
@@ -10,31 +10,40 @@ import javafx.stage.Stage;
 public class MainController {
 	@FXML
 	private Button addButton;
-	
+
 	@FXML
 	private Button testButton;
-	
+
 	@FXML
 	private void onClickAddButton() throws Exception {
 		Stage stage = (Stage) addButton.getScene().getWindow();
 		stage.close();
 		Stage primaryStage = new Stage();
-		Parent root = FXMLLoader.load(getClass().getResource("add-question-view.fxml"));
-		primaryStage.setTitle("Add question");
-		primaryStage.setScene(new Scene(root, 400, 600));
-		primaryStage.show();
+		if (Credentials.isLoggedIn() == true) {
+			Parent root = FXMLLoader.load(getClass().getResource("add-view.fxml"));
+			root.getStylesheets().add(getClass().getResource("/com/example/quizmanagement/styles.css").toExternalForm());
+			primaryStage.setTitle("Add question");
+			primaryStage.setScene(new Scene(root, 400, 600));
+			primaryStage.show();
+		} else {
+			Parent root = FXMLLoader.load(getClass().getResource("add-view.fxml"));
+			root.getStylesheets().add(getClass().getResource("/com/example/quizmanagement/styles.css").toExternalForm());
+			primaryStage.setTitle("Login");
+			primaryStage.setScene(new Scene(root, 400, 600));
+			primaryStage.show();
+		}
 	}
-	
+
 	@FXML
 	private void onClickTestButton() throws Exception {
 		Stage stage = (Stage) testButton.getScene().getWindow();
 		stage.close();
 		Stage primaryStage = new Stage();
 		Parent root = FXMLLoader.load(getClass().getResource("quiz-view.fxml"));
-		root.getStylesheets().add(getClass().getResource("/com/example/quizmanagement/quiz.css").toExternalForm());
+		root.getStylesheets().add(getClass().getResource("/com/example/quizmanagement/styles.css").toExternalForm());
 		primaryStage.setTitle("Test");
 		primaryStage.setScene(new Scene(root, 400, 600));
 		primaryStage.show();
 	}
-	
+
 }
