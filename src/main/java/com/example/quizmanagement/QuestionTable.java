f4e1f5a46b4ab770f6a3221b199ca8bce3264c3b
RobinSrk <robinsrk3@gmail.com>
2022-10-01 00:11:12 +0600

    added question table

--- src/main/java/com/example/quizmanagement/QuestionTable.java
+++ src/main/java/com/example/quizmanagement/QuestionTable.java
@@ -1,2 +1,33 @@
-package com.example.quizmanagement;public class QuestionTable {
+package com.example.quizmanagement;
+
+import javafx.scene.control.Button;
+
+public class QuestionTable {
+	private String question;
+	private Button delete;
+
+	public QuestionTable() {
+		this.question = "";
+	}
+
+	public QuestionTable(String question, Button delete) {
+		this.question = question;
+		this.delete = delete;
+	}
+
+	public String getQuestion() {
+		return question;
+	}
+
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+
+	public Button getDelete() {
+		return delete;
+	}
+
+	public void setDelete(Button delete) {
+		this.delete = delete;
+	}
 }
