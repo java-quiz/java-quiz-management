710ebab114d37efbac954507270a8edf52069dd5
RobinSrk <robinsrk3@gmail.com>
2022-10-03 06:55:23 +0600

    removed unused codes

--- src/main/java/com/example/quizmanagement/Credentials.java
+++ src/main/java/com/example/quizmanagement/Credentials.java
@@ -1,16 +1,7 @@
 package com.example.quizmanagement;
 
 public class Credentials {
-	private static boolean isLoggedIn = false;
 	private static String username;
-
-	public static boolean isLoggedIn() {
-		return isLoggedIn;
-	}
-
-	public static void setLoggedIn(boolean data) {
-		isLoggedIn = data;
-	}
 
 	public static String getUsername() {
 		return username;
